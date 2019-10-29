ca7c1a50928a824a7b5d80176dbc893c31a3d63e
Molly Johnson <johnsmol@flip1.engr.oregonstate.edu>
2019-10-29 16:13:07 -0700

    fixed citation for command line args and converting str to int

--- chatserve.py
+++ chatserve.py
@@ -6,11 +6,17 @@
 from socket import *
 import sys
 
-#using command line arguments using sys library and argv adapted from:
+#convert system arg 1 (string) to an int for the server port number
+#getting command line arguments using sys library and argv adapted from:
 #https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
+#converting a string to an int adapted from:
+#https://www.digitalocean.com/community/tutorials/how-to-convert-data-types-in-python-3
 serverPort = int(sys.argv[1]) 
 print('server port entered is: ' + str(serverPort))
+
+#specify host address to a flip server
 hostAddress = 'flip1.engr.oregonstate.edu'
+
 #basic socket server setup adapted from OSU CS 372 lecture 15 slides
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((hostAddress, serverPort))
