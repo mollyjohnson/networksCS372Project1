6d635a3b08dd4ca470747584ae33b787eedc0496
Molly Johnson <johnsmol@flip1.engr.oregonstate.edu>
2019-10-29 15:45:57 -0700

    added example python socket server

--- chatserve.py
+++ chatserve.py
@@ -2,3 +2,20 @@
 
 #
 
+from socket import *
+import sys
+
+serverPort = int(sys.argv[1]) 
+print('my arg entered is: ')
+print(serverPort)
+print('\n')
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive')
+while(1):
+	connectionSocket, addr = serverSocket.accept()
+	sentence = connectionSocket.recv(1024)
+	capitalizedSentence = sentence.upper()
+	connectionSocket.send(capitalizedSentence)
+	connectionSocket.close()
