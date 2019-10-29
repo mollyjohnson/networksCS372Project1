0842e47f9f836a6259bdea35d0bac8592fa069f6
Molly Johnson <johnsmol@oregonstate.edu>
2019-10-29 17:56:19 -0500

    with specific host tried out

--- chatserve.py
+++ chatserve.py
@@ -2,13 +2,18 @@
 
 #
 
+#imported libraries
 from socket import *
 import sys
 
+#using command line arguments using sys library and argv adapted from:
+#https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
 serverPort = int(sys.argv[1]) 
 print('my arg entered is: ' + str(serverPort))
+hostAddress = 'flip1.engr.oregonstate.edu'
+#basic socket server setup adapted from OSU CS 372 lecture 15 slides
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
+serverSocket.bind((hostAddress, serverPort))
 serverSocket.listen(1)
 print('The server is ready to receive')
 while(1):
