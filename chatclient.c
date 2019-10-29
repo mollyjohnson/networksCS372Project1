47898c62b16ad06ff7f45e4152145ec46e52ba9d
Molly Johnson <johnsmol@oregonstate.edu>
2019-10-29 13:08:28 -0500

    with C bones/outline to try makefile

---
+++ chatclient.c
@@ -0,0 +1,38 @@
+/*
+
+*/
+
+//include other header files
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+#include <dirent.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+
+//macro definitions
+
+//define true and false as C has no bool values
+#define FALSE 0
+#define TRUE 1
+
+/*
+
+*/
+int main(){
+    printf("hello world");
+
+    return 0;
+}
