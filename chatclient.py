a8765abc8665695f01933a16b314e0258effa2ac
Molly Johnson <johnsmol@oregonstate.edu>
2019-10-30 17:49:24 -0500

    testing w chatclient.py

--- chatclient.py
+++ chatclient.py
@@ -5,3 +5,13 @@
 #imported libraries
 from socket import *
 import sys
+
+serverPort = 34000
+hostAddress = 'flip1.engr.oregonstate.edu'
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((hostAddress, serverPort))
+sentence = input('Input lowercase sentence:')
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print('From Server: ', modifiedSentence.decode())
+clientSocket.close()
